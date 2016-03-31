c308cbec23eebd43580749b936e4a8d729bd7c33
sapphirinedreams <marilinsapphire@gmail.com>
2016-03-30 20:15:10 -0500

    Removed merge remnant

--- src/main.c
+++ src/main.c
@@ -110,9 +110,9 @@ static void tick_handler(struct tm *tick_time, TimeUnits changed) {
 	// debug = true;
 	if (debug) {
 		// use dummy time for emulator
-		s_last_time.seconds = 20;
-		s_last_time.hours = 6;
-		s_last_time.minutes = 15;
+		s_last_time.seconds = 0;
+		s_last_time.hours = 10;
+		s_last_time.minutes = 10;
 	} else {
 		s_last_time.hours = tick_time->tm_hour;
 		s_last_time.hours -= (s_last_time.hours > 12) ? 12 : 0;
@@ -177,31 +177,6 @@ static void bg_update_proc(Layer *layer, GContext *ctx) {
 	graphics_context_set_fill_color(ctx, gcolorbg);
 	graphics_fill_rect(ctx, bounds, 0, GCornerNone);
 	graphics_context_set_antialiased(ctx, ANTIALIASING);
-<<<<<<< 0d89ed62c9b17b47a165777ab9acd27136d53713
-
-	/*
-	graphics_context_set_stroke_color(ctx, gcolort);
-	for(int i = 0; i < 60; i++) {
-		int angle = (i * 360) / 60;
-
-		GRect outer_width = grect_inset(bounds, GEdgeInsets(0));
-		GPoint outer_edge = gpoint_from_polar(outer_width, GOvalScaleModeFitCircle, DEG_TO_TRIGANGLE(angle));
-
-		if (i % 5 != 0) {
-			graphics_context_set_stroke_width(ctx, 1);
-			GRect inner_width = grect_inset(bounds, GEdgeInsets(8));
-			GPoint inner_edge = gpoint_from_polar(inner_width, GOvalScaleModeFitCircle, DEG_TO_TRIGANGLE(angle));
-			graphics_draw_line(ctx, inner_edge, outer_edge);
-		} else {
-			graphics_context_set_stroke_width(ctx, 2);
-			GRect inner_width = grect_inset(bounds, GEdgeInsets(10));
-			GPoint inner_edge = gpoint_from_polar(inner_width, GOvalScaleModeFitCircle, DEG_TO_TRIGANGLE(angle));
-			graphics_draw_line(ctx, inner_edge, outer_edge);
-		}
-	}
-	*/
-=======
->>>>>>> Changed name and gitignore, removed commented ticks code.
 }
 
 static void shadow_update_proc(Layer *layer, GContext *ctx) {
